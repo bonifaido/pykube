0458496d2a8018618633966a20e1dc4493232f5d
Kracser Nandor <kracser.nandor@ustream.tv>
2016-11-08 14:56:12 +0100

    Exec

--- pykube/objects.py
+++ pykube/objects.py
@@ -277,6 +277,41 @@ class Pod(NamespacedAPIObject):
         r.raise_for_status()
         return r.text
 
+    def exec(self, container=None, command=None,
+             since_seconds=None, since_time=None, timestamps=False,
+             tail_lines=None, limit_bytes=None):
+        """
+        Produces the same result as calling kubectl exec pod/<pod-name>.
+        Check parameters meaning at
+        http://kubernetes.io/docs/api-reference/v1/operations/,
+        part 'read log of the specified Pod'. The result is plain text.
+        """
+        exec_call = "exec"
+        params = {}
+        if container is not None:
+            params["container"] = container
+        if command is not None:
+            params["command"] = command
+        if since_seconds is not None and since_time is None:
+            params["sinceSeconds"] = int(since_seconds)
+        elif since_time is not None and since_seconds is None:
+            params["sinceTime"] = since_time
+        if tail_lines is not None:
+            params["tailLines"] = int(tail_lines)
+        if limit_bytes is not None:
+            params["limitBytes"] = int(limit_bytes)
+
+        query_string = urlencode(params)
+        exec_call += "?{}".format(query_string) if query_string else ""
+        kwargs = {
+            "version": self.version,
+            "namespace": self.namespace,
+            "operation": exec_call,
+        }
+        r = self.api.get(**self.api_kwargs(**kwargs))
+        r.raise_for_status()
+        return r.text
+
 
 class ReplicationController(NamespacedAPIObject, ReplicatedMixin, ScalableMixin):
 
